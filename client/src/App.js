b1efb29c4e373e138758dc393b2471cddfa182a5
Sergey Teleshev <Dante.Angarsk@yandex.ru>
2020-07-16 14:37:59 +0300

    addSongToPlalist fix

--- client/src/App.js
+++ client/src/App.js
@@ -5,80 +5,103 @@ import SpotifyWebApi from 'spotify-web-api-js';
 const spotifyApi = new SpotifyWebApi();
 
 class App extends Component {
-  constructor()
-  {
-    super();
-    const params = this.getHashParams();
-    const token = params.access_token;
-    if (token) {
-      spotifyApi.setAccessToken(token);
+    constructor()
+    {
+        super();
+        const params = this.getHashParams();
+        const token = params.access_token;
+        if (token) {
+          spotifyApi.setAccessToken(token);
+        }
+        this.state = {
+          loggedIn: token ? true : false,
+          nowPlaying: { name: 'Not Checked', albumArt: '' },
+          foundSongs: [],
+        };
+
+        this.getSpotifySongId.bind(this);
     }
-    this.state = {
-      loggedIn: token ? true : false,
-      nowPlaying: { name: 'Not Checked', albumArt: '' },
-      foundSongs: [],
-    };
 
-    this.getSpotifySongId.bind(this);
-  }
-
-  getHashParams()
-  {
-    var hashParams = {};
-    var e, r = /([^&;=]+)=?([^&;]*)/g,
-        q = window.location.hash.substring(1);
-    e = r.exec(q)
-    while (e) {
-       hashParams[e[1]] = decodeURIComponent(e[2]);
-       e = r.exec(q);
+    getHashParams()
+    {
+        var hashParams = {};
+        var e, r = /([^&;=]+)=?([^&;]*)/g,
+            q = window.location.hash.substring(1);
+        e = r.exec(q)
+        while (e) {
+           hashParams[e[1]] = decodeURIComponent(e[2]);
+           e = r.exec(q);
+        }
+        return hashParams;
     }
-    return hashParams;
-  }
 
-  async getNowPlaying()
-  {
-    await spotifyApi.getMyCurrentPlaybackState()
-      .then((response) => {
-        this.setState({
-          nowPlaying: { 
-              name: response.item.name, 
-              albumArt: response.item.album.images[0].url
+    async getNowPlaying()
+    {
+        await spotifyApi.getMyCurrentPlaybackState()
+          .then((response) => {
+            this.setState({
+              nowPlaying: {
+                  name: response.item.name,
+                  albumArt: response.item.album.images[0].url
+                }
+            });
+          })
+    }
+
+    convertPlsToArray()
+    {
+        // 3 8 12
+        let plsText = document.getElementById("textFrom").value;
+        const splitedText = plsText.split("=");
+        console.log(splitedText);
+        let counter = 3;
+
+        let songs = splitedText.filter((element, index, array) => {
+            if(index + 5 < array.length && index % 5 === 0)
+            {
+                let songName = array[counter];
+                counter += 5;
+                return songName;
             }
         });
-      })
-  }
 
-  async getTrack(id)
-  {
-      let track = null;
-      await spotifyApi.getTrack(id).then(
+        console.log(songs);
+    }
+
+    async getTrack(id)
+    {
+        let track = null;
+        await spotifyApi.getTrack(id).then(
           data => {
               track = data;
           },
           err => {
 
           }
-      );
+        );
 
-      return track;
-  }
+        return track;
+    }
 
-  async addSongToLibrary(idArray)
-  {
-      const minAmountOfTracksToSend = 50;
-      let amountOfSmallArrays = Math.ceil(idArray.length / minAmountOfTracksToSend);
-      console.log(amountOfSmallArrays);
-      let currentIndex = 0;
+    async addSongToLibrary(idArray)
+    {
+        const minAmountOfTracksToSend = 49;
+        let amountOfSmallArrays = Math.ceil(idArray.length / minAmountOfTracksToSend);
+        console.log(amountOfSmallArrays);
+        let currentIndex = 0;
 
-      for(let i = 0; i < amountOfSmallArrays; i++)
-      {
+        for(let i = 0; i < amountOfSmallArrays; i++)
+        {
           let response = null;
           let arrayToSend = [];
 
-          if(currentIndex + minAmountOfTracksToSend > idArray.length)
-            arrayToSend = idArray.splice(currentIndex, idArray.length - 1);
+          if(currentIndex + minAmountOfTracksToSend >= idArray.length)
+            arrayToSend = idArray.slice(currentIndex, idArray.length - 1);
           else
-            arrayToSend = idArray.splice(currentIndex, currentIndex + minAmountOfTracksToSend - 1);
+            arrayToSend = idArray.slice(currentIndex, currentIndex + minAmountOfTracksToSend - 1);
+
+          console.log(arrayToSend);
+          console.log(currentIndex,currentIndex+minAmountOfTracksToSend, idArray.length);
 
           await spotifyApi.addToMySavedTracks(arrayToSend).then(
               data => {
@@ -91,32 +114,29 @@ class App extends Component {
 
           console.log(response);
           currentIndex += minAmountOfTracksToSend;
-      }
-  }
+        }
+    }
 
-  parseTxt()
-  {
-      let text = document.getElementById("textFrom").value;
-      let spitedText = text.split(",");
-      let songs = spitedText.filter((element, index, array) => {
+    parseTxt()
+    {
+        let text = document.getElementById("textFrom").value;
+        let spitedText = text.split(",");
+        let songs = spitedText.filter((element, index, array) => {
           if(index === 1)
               return element;
 
           if (index >= 3 && ((index - 2) % 2 !== 0))
               return array[index - 3];
-      });
+        });
 
-      console.log(songs);
+        return songs;
+    }
 
-      return songs;
-  }
-
-  async getSpotifySongId(text)
-  {
-      let songs = [];
-
-      await spotifyApi.searchTracks(text).then(
-          function (data) {
+    async getSpotifySongId(text)
+    {
+        let songs = [];
+        await spotifyApi.searchTracks(text).then(
+            function (data) {
               let spotifyItems = data.tracks.items;
 
               if(spotifyItems.length !== 0)
@@ -134,22 +154,23 @@ class App extends Component {
                       );
                   }
               }
-          },
-          function (err) {
+            },
+            function (err) {
               console.error(err);
-          }
-      );
+            }
+        );
 
-      console.log(songs);
-      if(songs.length > 0)
-          return songs[0].id;
-      else
-          return null
-  }
+        console.log(songs);
+        if(songs.length > 0 && songs[0].id && songs[0].id.length > 0)
+            return songs[0].id;
+        else
+            return null
+    }
 
     async convertSongsArrayToSpotifyLibrary()
     {
         const songs = this.parseTxt();
+        let nullCount = 0;
 
         let songsIds = [];
 
@@ -157,12 +178,18 @@ class App extends Component {
         {
             let songId = await this.getSpotifySongId(songs[i]);
             if (songId === null)
+            {
+                nullCount++;
                 continue;
+            }
 
             songsIds.push(songId);
         }
 
-        this.addSongToLibrary(songsIds);
+        console.log("songsIds", songsIds);
+        alert(nullCount);
+
+        await this.addSongToLibrary(songsIds);
     }
 
   render()
@@ -189,7 +216,7 @@ class App extends Component {
               <textarea id={"textFrom"}/>
           </div>
           <div>
-              <input type={"submit"} value={"parse text"} onClick={() => this.parseTxt()}/>
+              <input type={"submit"} value={"parse text"} onClick={() => this.convertPlsToArray()}/>
           </div>
           <div>
               <input type={"submit"} value={"All songs list to Spotify"} onClick={() => this.convertSongsArrayToSpotifyLibrary()}/>
